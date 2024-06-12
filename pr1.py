3ee65f457bfb47828619f0ae0ee5f3bbfe9e8b92
Dmitry B. <56137483+ajkelf@users.noreply.github.com>
2024-06-12 19:58:50 +0000

    Version 1.6: number of techniques

--- pr1.py
+++ pr1.py
@@ -1,13 +1,15 @@
 import json
 
 with open('techniques.json', 'r') as techniques_file:
-  data = techniques_file.read()
+  techniques_data = techniques_file.read()
 
-objects = json.loads(data)
+techniques_objects = json.loads(techniques_data)
+N = len(techniques_objects) #number of techniques
+print(f"Number of techniques: {N}")
 
 i = 1
-for technique in objects:
-  print(i + ". " + technique['id'])
+for technique in techniques_objects:
+  print(f"{i}. {technique['id']}")
   i = i + 1
   for command in technique['commands']:
     print(command)
